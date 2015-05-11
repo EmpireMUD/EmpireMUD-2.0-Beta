f6535bf4cca69f51c9ce9bb5cf9fec06df4c9524
EmpireMUD <paul@empiremud.net>
2015-05-11 08:52:01 -0700

    Enrage message fix
    Should no longer alert if it can’t attack.

--- src/limits.c
+++ src/limits.c
@@ -634,10 +634,13 @@ void real_update_char(char_data *ch) {
 		}
 		
 		if (++fol_count > config_get_int("npc_follower_limit")) {
-			act("$n becomes enraged!", FALSE, room_ch, NULL, NULL, TO_ROOM);
 			REMOVE_BIT(AFF_FLAGS(room_ch), AFF_CHARM);
 			stop_follower(room_ch);
-			engage_combat(room_ch, ch, TRUE);
+			
+			if (can_fight(room_ch, ch)) {
+				act("$n becomes enraged!", FALSE, room_ch, NULL, NULL, TO_ROOM);
+				engage_combat(room_ch, ch, TRUE);
+			}
 		}
 	}
 
