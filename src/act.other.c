cb969b5580c9b1b50001c0b4b7a229f5e3974e63
EmpireMUD <paul@empiremud.net>
2015-08-20 17:16:38 -0700

    Variable fix.

--- src/act.other.c
+++ src/act.other.c
@@ -346,7 +346,7 @@ static void print_group(char_data *ch) {
 			
 			// show location if different
 			if (IN_ROOM(k) != IN_ROOM(ch)) {
-				if (HAS_ABILITY(ch, ABIL_NAVIGATION) && X_COORD(IN_ROOM(ch)) >= 0) {
+				if (HAS_ABILITY(ch, ABIL_NAVIGATION) && X_COORD(IN_ROOM(k)) >= 0) {
 					snprintf(loc, sizeof(loc), " - %s (%d, %d)", get_room_name(IN_ROOM(k), FALSE), X_COORD(IN_ROOM(k)), Y_COORD(IN_ROOM(k)));
 				}
 				else {
