f739cd17eca1025b9a297a600810f2b3160a92e0
EmpireMUD <paul@empiremud.net>
2015-12-29 23:24:28 -0700

    fix for led-by

--- src/act.informative.c
+++ src/act.informative.c
@@ -875,7 +875,10 @@ void list_one_vehicle_to_char(vehicle_data *veh, char_data *ch) {
 	size += snprintf(buf + size, sizeof(buf) - size, "%s\r\n", VEH_LONG_DESC(veh));
 	
 	// additional descriptions like what's attached:
-	if (VEH_LED_BY(veh)) {
+	if (VEH_LED_BY(veh) == ch) {
+		size += snprintf(buf + size, sizeof(buf) - size, "...you are leading it.\r\n");
+	}
+	else if (VEH_LED_BY(veh)) {
 		size += snprintf(buf + size, sizeof(buf) - size, "...it is being led by %s.\r\n", PERS(VEH_LED_BY(veh), ch, FALSE));
 	}
 	if (VEH_ANIMALS(veh)) {
