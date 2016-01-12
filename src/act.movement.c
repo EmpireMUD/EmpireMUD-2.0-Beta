f5bc594af301100afa65b68e6b5d35b140930855
EmpireMUD <paul@empiremud.net>
2016-01-11 20:55:45 -0700

    move fix

--- src/act.movement.c
+++ src/act.movement.c
@@ -508,7 +508,7 @@ int can_move(char_data *ch, int dir, room_data *to_room, int need_specials_check
 		// sends own messages
 		return 0;
 	}
-	if (GET_LEADING_MOB(ch) && !GET_LEADING_MOB(ch)->desc && IN_ROOM(GET_LEADING_MOB(ch)) == IN_ROOM(ch) && !can_move(GET_LEADING_MOB(ch), dir, to_room, FALSE)) {
+	if (GET_LEADING_MOB(ch) && !GET_LEADING_MOB(ch)->desc && IN_ROOM(GET_LEADING_MOB(ch)) == IN_ROOM(ch) && !can_move(GET_LEADING_MOB(ch), dir, to_room, TRUE)) {
 		act("You can't go there while leading $N.", FALSE, ch, NULL, GET_LEADING_MOB(ch), TO_CHAR);
 		return 0;
 	}
