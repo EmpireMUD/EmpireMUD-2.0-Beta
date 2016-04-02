f2f41b9493bb630719d495ecc489e6a3037cf17d
EmpireMUD <paul@empiremud.net>
2016-04-02 15:20:55 -0700

    workforce requires storable resource

--- src/workforce.c
+++ src/workforce.c
@@ -542,19 +542,29 @@ static bool can_gain_chore_resource(empire_data *emp, room_data *loc, int chore,
 bool can_gain_chore_resource_from_interaction_list(empire_data *emp, room_data *location, int chore, struct interaction_item *list, int interaction_type, bool highest_only) {
 	struct interaction_item *interact, *found = NULL;
 	double best_percent = 0.0;
+	obj_data *proto;
 	
 	for (interact = list; interact; interact = interact->next) {
-		if (interact->type == interaction_type) {
-			if (highest_only) {
-				if (!found || interact->percent > best_percent) {
-					best_percent = interact->percent;
-					found = interact;
-				}
+		if (interact->type != interaction_type) {
+			continue;
+		}
+		if (!(proto = obj_proto(interact->vnum))) {
+			continue;
+		}
+		if (!proto->storage) {
+			continue;	// MUST be storable
+		}
+		
+		// found
+		if (highest_only) {
+			if (!found || interact->percent > best_percent) {
+				best_percent = interact->percent;
+				found = interact;
 			}
-			else if (can_gain_chore_resource(emp, location, chore, interact->vnum)) {
-				// any 1 is fine
-				return TRUE;
-			}
+		}
+		else if (can_gain_chore_resource(emp, location, chore, interact->vnum)) {
+			// any 1 is fine
+			return TRUE;
 		}
 	}
 	
