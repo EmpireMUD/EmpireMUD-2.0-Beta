3ba852b3d14e1b4f5aa235cbd39c2f6c17e21872
EmpireMUD <paul@empiremud.net>
2016-05-16 11:19:13 -0700

    check_sunny fix

--- src/utils.c
+++ src/utils.c
@@ -4032,7 +4032,7 @@ sector_data *get_sect_by_name(char *name) {
 * @return bool TRUE if it's sunny there.
 */
 bool check_sunny(room_data *room) {
-	bool sun_sect = !ROOM_IS_CLOSED(room);
+	bool sun_sect = !ROOM_IS_CLOSED(room) || (IS_ADVENTURE_ROOM(room) && RMT_FLAGGED(room, RMT_OUTDOOR));
 	return (sun_sect && weather_info.sunlight != SUN_DARK && !ROOM_AFF_FLAGGED(room, ROOM_AFF_DARK));
 }
 
