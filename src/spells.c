7ec2b49dda9e643430fc9a62ebcac9024eca5cd7
EmpireMUD <paul@empiremud.net>
2016-06-04 16:30:36 -0700

    damage spell update

--- src/spells.c
+++ src/spells.c
@@ -63,41 +63,50 @@ bool trigger_counterspell(char_data *ch) {
 
 struct damage_spell_type {
 	any_vnum ability;	// ABIL_ type
-	int cost;	// mana
-	int attack_type;	// ATTACK_x
+	double cost_mod;	// mana cost as a % of normal spells (1.0 = normal)
+	int attack_type;	// ATTACK_
 	double damage_mod;	// 1.0 = normal damage, balance based on affects
-	bitvector_t aff_immunity;	// AFF_x flag making person immune
+	bitvector_t aff_immunity;	// AFF_ flag making person immune
 	
 	// affect group: all this only matters if aff_type != -1
 	int aff_type;	// ATYPE_, -1 for none
 	int duration;	// time for the affect
 	int apply;	// APPLY_, 0 for none
 	int modifier;	// +/- value, if apply != 0
-	bitvector_t aff_flag;	// AFF_x, 0 for none
+	bitvector_t aff_flag;	// AFF_, 0 for none
 	
 	// dot affect
 	int dot_type;	// ATYPE_, -1 for none
 	int dot_duration;	// time for the dot
-	int dot_damage_type;	// DAM_x for the dot
-	int dot_damage;	// damage for the dot
+	int dot_damage_type;	// DAM_ for the dot
+	double dot_dmg_mod;	// % of scaled dot damage (1.0 = normal)
 	int dot_max_stacks;	// how high the dot can stack
 	
-	int cooldown_type;	// COOLDOWN_x
+	int cooldown_type;	// COOLDOWN_
 	int cooldown_time;	// seconds
 };
 
+// shortcuts
+#define NO_SPELL_AFFECT  -1, 0, 0, 0, NOBITS
+#define NO_DOT_AFFECT  -1, 0, 0, 0, 0
+
 const struct damage_spell_type damage_spell[] = {
+	// ABIL_, cost-mod, ATTACK_, damage-mod, immunity-aff,
+	// affect (optional): ATYPE_, duration, apply, modifier, adds-aff
+	// dot (optional): ATYPE_, duration, DAM_, damage, max-stacks
+	// COOLDOWN_, cooldown
+	
 	// Lightningbolt
-	{ ABIL_LIGHTNINGBOLT, 25, ATTACK_LIGHTNINGBOLT, 0.8, AFF_IMMUNE_NATURAL_MAGIC,
-		-1, 0, 0, 0, NOBITS,
-		ATYPE_SHOCKED, 3, DAM_MAGICAL, 5, 1,
+	{ ABIL_LIGHTNINGBOLT, 1.0, ATTACK_LIGHTNINGBOLT, 0.8, AFF_IMMUNE_NATURAL_MAGIC,
+		NO_SPELL_AFFECT,
+		ATYPE_SHOCKED, 3, DAM_MAGICAL, 0.5, 1,
 		COOLDOWN_LIGHTNINGBOLT, 9
 	},
 	
 	// SUNSHOCK
-	{ ABIL_SUNSHOCK, 25, ATTACK_SUNSHOCK, 0.6, AFF_IMMUNE_HIGH_SORCERY,
+	{ ABIL_SUNSHOCK, 1.3, ATTACK_SUNSHOCK, 0.6, AFF_IMMUNE_HIGH_SORCERY,
 		ATYPE_SUNSHOCK, 1, APPLY_NONE, 0, AFF_BLIND,
-		-1, 0, 0, 0, 0,
+		NO_DOT_AFFECT,
 		COOLDOWN_SUNSHOCK, 9
 	},
 	
@@ -111,9 +120,9 @@ const struct damage_spell_type damage_spell[] = {
 ACMD(do_damage_spell) {
 	char_data *vict = NULL;
 	struct affected_type *af;
-	int iter, type = NOTHING, cost, dmg, result;
+	int iter, type = NOTHING, cost, dmg, dot_dmg, result;
 	
-	double level_mod[] = { 1.0, 1.5, 2.0 };
+	double cost_mod[] = { 1.5, 1.2, 0.9 };
 	
 	// find ability
 	for (iter = 0; damage_spell[iter].ability != NO_ABIL; ++iter) {
@@ -130,9 +139,43 @@ ACMD(do_damage_spell) {
 		return;
 	}
 	
-	// cost calculations
-	cost = damage_spell[type].cost;
+	// calculate damage in order to calculate cost
+	if ((IS_NPC(ch) || GET_CLASS_ROLE(ch) == ROLE_CASTER || GET_CLASS_ROLE(ch) == ROLE_SOLO) && check_solo_role(ch)) {
+		dmg = get_approximate_level(ch) / 8.0;
+		dmg += GET_BONUS_MAGICAL(ch);
+	}
+	else {	// not on a role
+		dmg = get_ability_level(ch, subcmd) / 8.0;
+	}
 	
+	dmg += GET_INTELLIGENCE(ch);	// both add this
+	dmg *= damage_spell[type].damage_mod;	// modify by the spell
+	
+	// calculate DoT damage if any
+	dot_dmg = 0;
+	if (damage_spell[type].dot_type > 0) {
+		dot_dmg = get_ability_level(ch, subcmd) / 24;	// skill level base
+		if ((IS_NPC(ch) || GET_CLASS_ROLE(ch) == ROLE_CASTER || GET_CLASS_ROLE(ch) == ROLE_SOLO) && check_solo_role(ch)) {
+			dot_dmg = MAX(dot_dmg, (get_approximate_level(ch) / 24));	// total level base
+			dot_dmg += GET_BONUS_MAGICAL(ch) / MAX(1, damage_spell[type].dot_duration);
+		}
+		
+		dot_dmg += GET_INTELLIGENCE(ch) / MAX(1, damage_spell[type].dot_duration);	// always add int
+		
+		// finally:
+		dot_dmg = round(dot_dmg * damage_spell[type].dot_dmg_mod);
+		dot_dmg = MAX(1, dot_dmg);
+	}
+	
+	// cost calculations
+	cost = round((dmg + dot_dmg) * CHOOSE_BY_ABILITY_LEVEL(cost_mod, ch, ABIL_SKYBRAND) * damage_spell[type].cost_mod);
+	
+	// special-casing (done AFTER cost)
+	if (damage_spell[type].ability == ABIL_SUNSHOCK && IS_VAMPIRE(vict)) {
+		dmg *= 1.5;
+	}
+	
+	// check ability and cost
 	if (!can_use_ability(ch, subcmd, MANA, cost, damage_spell[type].cooldown_type)) {
 		return;
 	}
@@ -162,14 +205,6 @@ ACMD(do_damage_spell) {
 		return;
 	}
 	
-	dmg = get_ability_level(ch, subcmd) / 8.0 * damage_spell[type].damage_mod;
-	dmg += GET_INTELLIGENCE(ch) * CHOOSE_BY_ABILITY_LEVEL(level_mod, ch, subcmd);
-	dmg += GET_BONUS_MAGICAL(ch);
-	
-	if (damage_spell[type].ability == ABIL_SUNSHOCK && IS_VAMPIRE(vict)) {
-		dmg *= 2;
-	}
-	
 	charge_ability_cost(ch, MANA, cost, damage_spell[type].cooldown_type, damage_spell[type].cooldown_time, WAIT_COMBAT_SPELL);
 	
 	if (SHOULD_APPEAR(ch)) {
@@ -186,8 +221,8 @@ ACMD(do_damage_spell) {
 				af = create_aff(damage_spell[type].aff_type, damage_spell[type].duration, damage_spell[type].apply, damage_spell[type].modifier, damage_spell[type].aff_flag, ch);
 				affect_join(vict, af, 0);
 			}
-			if (damage_spell[type].dot_type > 0) {
-				apply_dot_effect(vict, damage_spell[type].dot_type, damage_spell[type].dot_duration, damage_spell[type].dot_damage_type, damage_spell[type].dot_damage, damage_spell[type].dot_max_stacks, ch);
+			if (damage_spell[type].dot_type > 0 && dot_dmg > 0) {
+				apply_dot_effect(vict, damage_spell[type].dot_type, damage_spell[type].dot_duration, damage_spell[type].dot_damage_type, dot_dmg, damage_spell[type].dot_max_stacks, ch);
 			}
 		}
 	}
