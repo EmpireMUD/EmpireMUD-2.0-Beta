43d92def3b1a20804af800397746084d791bc1e8
EmpireMUD <paul@empiremud.net>
2016-11-06 13:03:25 -0700

    naturalize, remember, and newbie islands

--- src/olc.c
+++ src/olc.c
@@ -201,7 +201,9 @@ OLC_MODULE(mapedit_delete_room);
 OLC_MODULE(mapedit_exits);
 OLC_MODULE(mapedit_icon);
 OLC_MODULE(mapedit_maintain);
+OLC_MODULE(mapedit_naturalize);
 OLC_MODULE(mapedit_pass_walls);
+OLC_MODULE(mapedit_remember);
 OLC_MODULE(mapedit_room_description);
 OLC_MODULE(mapedit_room_name);
 OLC_MODULE(mapedit_roomtype);
@@ -609,7 +611,9 @@ const struct olc_command_data olc_data[] = {
 	{ "icon", mapedit_icon, OLC_MAP, OLC_CF_MAP_EDIT },
 	{ "maintain", mapedit_maintain, OLC_MAP, OLC_CF_MAP_EDIT },
 	{ "name", mapedit_room_name, OLC_MAP, OLC_CF_MAP_EDIT },
+	{ "naturalize", mapedit_naturalize, OLC_MAP, OLC_CF_MAP_EDIT },
 	{ "passwalls", mapedit_pass_walls, OLC_MAP, OLC_CF_MAP_EDIT },
+	{ "remember", mapedit_remember, OLC_MAP, OLC_CF_MAP_EDIT },
 	{ "roomtype", mapedit_roomtype, OLC_MAP, OLC_CF_MAP_EDIT },
 	{ "ruin", mapedit_ruin, OLC_MAP, OLC_CF_MAP_EDIT },
 	{ "terrain", mapedit_terrain, OLC_MAP, OLC_CF_MAP_EDIT },
