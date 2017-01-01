b934d89752ea8b1f0adbc389aab6af2c6fcdd61c
EmpireMUD <paul@empiremud.net>
2017-01-01 09:51:59 -0700

    free carry quest objs

--- src/act.item.c
+++ src/act.item.c
@@ -525,7 +525,7 @@ int obj_carry_size(obj_data *obj) {
 	int size = 0;
 	
 	// my size
-	if (IS_COINS(obj)) {
+	if (IS_COINS(obj) || GET_OBJ_REQUIRES_QUEST(obj)) {
 		size = 0;
 	}
 	else if (OBJ_FLAGGED(obj, OBJ_LARGE)) {
