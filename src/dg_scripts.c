b88bf3be4e400b386fbcf5900a7531542d014f0b
EmpireMUD <paul@empiremud.net>
2017-02-22 14:29:32 -0700

    scripting fix

--- src/dg_scripts.c
+++ src/dg_scripts.c
@@ -2309,7 +2309,7 @@ void find_replacement(void *go, struct script_data *sc, trig_data *trig, int typ
 					room_data *find;
 					any_vnum vnum;
 					if (subfield && isdigit(*subfield) && ((vnum = atoi(subfield)) != NOTHING) && (find = find_nearest_rmt(IN_ROOM(ch), vnum))) {
-						snprintf(str, slen, "%c%d", UID_CHAR, GET_ROOM_VNUM(inst->location) + ROOM_ID_BASE);
+						snprintf(str, slen, "%c%d", UID_CHAR, GET_ROOM_VNUM(find) + ROOM_ID_BASE);
 					}
 					else {
 						snprintf(str, slen, "0");
