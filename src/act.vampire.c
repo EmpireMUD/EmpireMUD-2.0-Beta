e9ea7ef387c96c426dde9865b5592f40f3d8b32f
Awkens Ryder <joaofgbraz@gmail.com>
2017-03-25 17:42:10 +0000

    Fixed a bug where you could feed yourself.

--- src/act.vampire.c
+++ src/act.vampire.c
@@ -1096,6 +1096,8 @@ ACMD(do_feed) {
 		msg_to_char(ch, "You can't give THAT much blood.\r\n");
 	else if (!(victim = get_char_vis(ch, arg, FIND_CHAR_ROOM)))
 		send_config_msg(ch, "no_person");
+	else if ( ch == victim )
+		msg_to_char(ch, "What would be the point in that?\r\n");
 	else if (IS_DEAD(victim)) {
 		act("It would do no good for $M now -- $E's dead!", FALSE, ch, NULL, victim, TO_CHAR);
 	}
