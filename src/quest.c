aaedd862869f3de07cabaa50ae5d7bfea8844bd1
EmpireMUD <paul@empiremud.net>
2017-05-16 14:59:19 -0700

    text updates

--- src/quest.c
+++ src/quest.c
@@ -3932,7 +3932,12 @@ OLC_MODULE(qedit_rewards) {
 				if (--num == 0) {
 					found = TRUE;
 					
-					msg_to_char(ch, "You remove the reward for %dx %s %d.\r\n", iter->amount, quest_reward_types[iter->type], iter->vnum);
+					if (iter->vnum > 0) {
+						msg_to_char(ch, "You remove the reward for %dx %s %d.\r\n", iter->amount, quest_reward_types[iter->type], iter->vnum);
+					}
+					else {
+						msg_to_char(ch, "You remove the reward for %dx %s.\r\n", iter->amount, quest_reward_types[iter->type]);
+					}
 					LL_DELETE(*list, iter);
 					free(iter);
 					break;
