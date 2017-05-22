4eafdc939c88b35e8cb52a8651426d7e0bd25058
EmpireMUD <paul@empiremud.net>
2017-05-22 07:09:49 -0700

    color for screenreaders

--- src/protocol.c
+++ src/protocol.c
@@ -1653,7 +1653,7 @@ const char *ColourRGB(descriptor_t *apDescriptor, const char *apRGB, const char 
 		return ansiBackup;
 	}
 
-	if (pProtocol && pProtocol->pVariables[eMSDP_ANSI_COLORS]->ValueInt && (!apDescriptor->character || !PRF_FLAGGED(REAL_CHAR(apDescriptor->character), PRF_SCREEN_READER))) {
+	if (pProtocol && pProtocol->pVariables[eMSDP_ANSI_COLORS]->ValueInt) {
 		if (IsValidColour(apRGB)) {
 			bool_t bBackground = (tolower(apRGB[0]) == 'b');
 			int Red = apRGB[1] - '0';
