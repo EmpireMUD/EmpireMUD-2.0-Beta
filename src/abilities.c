4df559c5d562c1be8a0b8819aa50f2bf804ae514
EmpireMUD <paul@empiremud.net>
2017-10-30 09:06:09 -0700

    remove debugs

--- src/abilities.c
+++ src/abilities.c
@@ -1025,35 +1025,27 @@ void call_ability(char_data *ch, ability_data *abil, char *argument, char_data *
 	}
 	*/
 	
-	msg_to_char(ch, "test 1\r\n");
 	if (data->success) {
-		msg_to_char(ch, "test 2\r\n");
 		// experience
 		if (!cvict || can_gain_exp_from(ch, cvict)) {
 			gain_ability_exp(ch, ABIL_VNUM(abil), 15);
-			msg_to_char(ch, "test 3\r\n");
 		}
 		
 		// check if should be in combat
 		if (cvict && cvict != ch && !ABILITY_FLAGGED(abil, ABILF_NO_ENGAGE) && !EXTRACTED(cvict) && !IS_DEAD(cvict)) {
-			msg_to_char(ch, "test 4\r\n");
 			// auto-assist if we used an ability on someone who is fighting
 			if (!violent && FIGHTING(cvict) && !FIGHTING(ch)) {
 				engage_combat(ch, FIGHTING(cvict), ABILITY_FLAGGED(abil, ABILF_RANGED | ABILF_RANGED_ONLY) ? FALSE : TRUE);
-				msg_to_char(ch, "test 5\r\n");
 			}
 			
 			// auto-attack if used on an enemy
 			if (violent && CAN_SEE(cvict, ch) && !FIGHTING(cvict)) {
 				engage_combat(cvict, ch, ABILITY_FLAGGED(abil, ABILF_RANGED | ABILF_RANGED_ONLY) ? FALSE : TRUE);
-				msg_to_char(ch, "test 6\r\n");
 			}
 		}
-		msg_to_char(ch, "test 7\r\n");
 	}
 	else if (!data->no_msg) {
 		msg_to_char(ch, "It doesn't seem to have any effect.\r\n");
-		msg_to_char(ch, "test 8\r\n");
 	}
 }
 
