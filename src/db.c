a4051cd24604e3353664cc42d930bc5043e5ea2e
EmpireMUD <paul@empiremud.net>
2017-12-07 10:03:29 -0700

    remove incorrect removal flag

--- src/db.c
+++ src/db.c
@@ -2741,7 +2741,7 @@ void b5_19_world_fix(void) {
 	room_data *room;
 	bld_data *bld;
 	
-	bitvector_t flags_to_wipe = ROOM_AFF_DARK | ROOM_AFF_SILENT | ROOM_AFF_CHAMELEON | ROOM_AFF_NO_WEATHER | ROOM_AFF_NO_TELEPORT;
+	bitvector_t flags_to_wipe = ROOM_AFF_DARK | ROOM_AFF_SILENT | ROOM_AFF_NO_WEATHER | ROOM_AFF_NO_TELEPORT;
 	bitvector_t empire_only_flags = ROOM_AFF_PUBLIC | ROOM_AFF_NO_WORK | ROOM_AFF_NO_DISMANTLE;
 	
 	LL_FOREACH(land_map, map) {
