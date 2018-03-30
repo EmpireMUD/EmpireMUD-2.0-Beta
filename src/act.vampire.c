4ea7e72935d5c36a40943d3765a6aa5245d6d4aa
EmpireMUD <paul@empiremud.net>
2018-03-30 08:24:39 -0700

    Revert "real age fix"
    This reverts commit da4ad436f6c218ef5141bca1063d7037829c3a6c.

--- src/act.vampire.c
+++ src/act.vampire.c
@@ -222,7 +222,7 @@ void make_vampire(char_data *ch, bool lore) {
 	
 	if (!IS_NPC(ch)) {
 		/* set BEFORE set as a vampire! */
-		ch->player_specials->apparent_age = GET_REAL_AGE(ch);
+		GET_APPARENT_AGE(ch) = GET_REAL_AGE(ch);
 		
 		if (get_skill_level(ch, SKILL_VAMPIRE) < 1) {
 			gain_skill(ch, find_skill_by_vnum(SKILL_VAMPIRE), 1);
