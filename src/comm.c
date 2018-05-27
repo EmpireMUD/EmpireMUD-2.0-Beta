420e780480f64006557c93a37b4b91808332c60f
Lucien Sadi <luciensadi@gmail.com>
2018-05-27 10:13:24 -0700

    Compilation fix for systems missing OPEN_MAX

--- src/comm.c
+++ src/comm.c
@@ -1888,6 +1888,9 @@ int get_max_players(void) {
 		}
 
 		/* set the current to the maximum */
+#ifndef OPEN_MAX
+#def OPEN_MAX limit.rlim_max
+#endif
 		limit.rlim_cur = MIN(OPEN_MAX, limit.rlim_max);
 		if (setrlimit(RLIMIT_NOFILE, &limit) < 0) {
 			perror("SYSERR: calling setrlimit");
