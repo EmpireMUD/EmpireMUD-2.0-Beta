636d3442128cbc997948e25538121b5acaa21a77
EmpireMUD <paul@empiremud.net>
2018-09-07 07:32:26 -0700

    mob fullsearch

--- src/olc.c
+++ src/olc.c
@@ -998,7 +998,7 @@ const struct olc_command_data olc_data[] = {
 	
 	
 	// misc commands that should not take precedence over editor commands
-	{ "fullsearch", olc_fullsearch, OLC_ABILITY | OLC_OBJECT | OLC_TRIGGER, NOBITS },
+	{ "fullsearch", olc_fullsearch, OLC_ABILITY | OLC_MOBILE |  OLC_OBJECT | OLC_TRIGGER, NOBITS },
 	
 	// this goes last
 	{ "\n", NULL, NOBITS, NOBITS }
@@ -2535,6 +2535,11 @@ OLC_MODULE(olc_fullsearch) {
 		case OLC_ABILITY: {
 			void olc_fullsearch_abil(char_data *ch, char *argument);
 			olc_fullsearch_abil(ch, argument);
+			break;
+		}
+		case OLC_MOBILE: {
+			void olc_fullsearch_mob(char_data *ch, char *argument);
+			olc_fullsearch_mob(ch, argument);
 			break;
 		}
 		case OLC_OBJECT: {
