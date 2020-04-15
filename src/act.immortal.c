83daddfbe75fe4866f6ec7211699c7b75f22e512
EmpireMUD <paul@empiremud.net>
2020-04-14 21:29:07 -0700

    color tweak

--- src/act.immortal.c
+++ src/act.immortal.c
@@ -5867,10 +5867,10 @@ void do_stat_room(char_data *ch) {
 	
 	// sector data
 	if (ROOM_SECT_FLAGGED(IN_ROOM(ch), SECTF_HAS_CROP_DATA) && (cp = ROOM_CROP(IN_ROOM(ch)))) {
-		msg_to_char(ch, "Crop: [\tc%d\t0 - \tc%s\t0], ", GET_CROP_VNUM(cp), GET_CROP_NAME(cp));
+		msg_to_char(ch, "Crop: [\to%d\t0 - \to%s\t0], ", GET_CROP_VNUM(cp), GET_CROP_NAME(cp));
 	}
 	msg_to_char(ch, "Sector: [\tc%d\t0 - \tc%s\t0]", GET_SECT_VNUM(SECT(IN_ROOM(ch))), GET_SECT_NAME(SECT(IN_ROOM(ch))));
-	msg_to_char(ch, ", Base: [\ty%d\t0 - \tc=y%s\t0]", GET_SECT_VNUM(BASE_SECT(IN_ROOM(ch))), GET_SECT_NAME(BASE_SECT(IN_ROOM(ch))));
+	msg_to_char(ch, ", Base: [\ty%d\t0 - \ty%s\t0]", GET_SECT_VNUM(BASE_SECT(IN_ROOM(ch))), GET_SECT_NAME(BASE_SECT(IN_ROOM(ch))));
 	if (GET_ROOM_VNUM(IN_ROOM(ch)) < MAP_SIZE && (map = &world_map[X_COORD(IN_ROOM(ch))][Y_COORD(IN_ROOM(ch))])) {
 		msg_to_char(ch, ", Natural: [\tg%d\t0 - \tg%s\t0]", GET_SECT_VNUM(map->natural_sector), GET_SECT_NAME(map->natural_sector));
 	}
