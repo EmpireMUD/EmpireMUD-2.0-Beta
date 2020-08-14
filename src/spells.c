e66db4cacd6d970c510f78f0d476e9e2f692f962
EmpireMUD <paul@empiremud.net>
2020-08-14 09:47:26 -0700

    new do_ready command

--- src/spells.c
+++ src/spells.c
@@ -357,70 +357,141 @@ struct ready_magic_weapon_type {
 ACMD(do_ready) {
 	extern bool check_vampire_sun(char_data *ch, bool message);
 	void scale_item_to_level(obj_data *obj, int level);
+	extern const char *pool_types[];
 	
-	ability_data *abil;
-	obj_data *obj;
-	int type, iter, scale_level, ch_level = 0;
-	bool found, later = TRUE;
+	char buf[MAX_STRING_LENGTH * 2], line[MAX_STRING_LENGTH];
+	struct player_ability_data *plab, *next_plab;
+	int scale_level, ch_level = 0, pos;
+	ability_data *abil, *found_abil;
+	bool found, full, later = TRUE;
+	struct ability_data_list *adl;
+	obj_data *obj, *proto;
+	size_t size, lsize;
 	
-	one_argument(argument, arg);
+	skip_spaces(&argument);
 	
 	if (IS_NPC(ch)) {
 		msg_to_char(ch, "NPCs can't use ready.\r\n");
 		return;
 	}
 	
-	if (!*arg) {
-		msg_to_char(ch, "You know how to ready the following weapons: ");
+	#define VALID_READY_ABIL(ch, plab, abil)  ((abil) && (plab) && (plab)->purchased[GET_CURRENT_SKILL_SET(ch)] && IS_SET(ABIL_TYPES(abil), ABILT_READY_WEAPONS))
+	
+	if (!*argument) {
+		size = snprintf(buf, sizeof(buf), "You know how to ready the following weapons:\r\n");
 		
-		found = FALSE;
-		for (iter = 0; *ready_magic_weapon[iter].name != '\n'; ++iter) {
-			if (ready_magic_weapon[iter].ability == NO_ABIL || has_ability(ch, ready_magic_weapon[iter].ability)) {
-				msg_to_char(ch, "%s%s", (found ? ", " : ""), ready_magic_weapon[iter].name);
-				found = TRUE;
+		found = full = FALSE;
+		HASH_ITER(hh, GET_ABILITY_HASH(ch), plab, next_plab) {
+			abil = plab->ptr;
+			if (!VALID_READY_ABIL(ch, plab, abil)) {
+				continue;
+			}
+			
+			LL_FOREACH(ABIL_DATA(abil), adl) {
+				if (adl->type == ADL_READY_WEAPON && obj_proto(adl->vnum)) {
+					// build display
+					lsize = snprintf(line, sizeof(line), " %s", skip_filler(get_obj_name_by_proto(adl->vnum)));
+					
+					if (ABIL_COST(abil) > 0) {
+						lsize += snprintf(line + lsize, sizeof(line) - lsize, " (%d %s)", ABIL_COST(abil), pool_types[ABIL_COST_TYPE(abil)]);
+					}
+					
+					strcat(line, "\r\n");
+					lsize += 2;
+					
+					if (size + lsize < sizeof(buf)) {
+						strcat(buf, line);
+						size += lsize;
+						full = TRUE;
+						break;
+					}
+				}
+			}
+			
+			if (full) {
+				break;
 			}
 		}
 		
-		msg_to_char(ch, "%s\r\n", (found ? "" : "none"));
+		if (!found) {
+			strcat(buf, " none\r\n");	// always room for this if !found
+		}
+		if (full) {
+			snprintf(buf + size, sizeof(buf) - size, "OVERFLOW\r\n");
+		}
+		if (ch->desc) {
+			page_string(ch->desc, buf, TRUE);
+		}
 		return;
 	}
 	
 	// lookup
 	found = FALSE;
-	for (iter = 0; *ready_magic_weapon[iter].name != '\n' && !found; ++iter) {
-		if (is_abbrev(arg, ready_magic_weapon[iter].name) && (ready_magic_weapon[iter].ability == NO_ABIL || has_ability(ch, ready_magic_weapon[iter].ability))) {
-			type = iter;
-			found = TRUE;
+	found_abil = NULL;
+	HASH_ITER(hh, GET_ABILITY_HASH(ch), plab, next_plab) {
+		abil = plab->ptr;
+		if (!VALID_READY_ABIL(ch, plab, abil)) {
+			continue;
+		}
+		
+		LL_FOREACH(ABIL_DATA(abil), adl) {
+			if (adl->type == ADL_READY_WEAPON && (proto = obj_proto(adl->vnum))) {
+				if (multi_isname(argument, GET_OBJ_KEYWORDS(proto))) {
+					found = TRUE;
+					found_abil = abil;
+					break;
+				}
+			}
+		}
+		
+		if (found) {
+			break;
 		}
 	}
 	
 	// validate
-	if (!found) {
+	if (!found || !proto || !found_abil) {
 		msg_to_char(ch, "You don't know how to ready that.\r\n");
 		return;
 	}
-	if (!can_use_ability(ch, ready_magic_weapon[type].ability, ready_magic_weapon[type].cost_pool, ready_magic_weapon[type].cost, NOTHING)) {
+	if (!can_use_ability(ch, ABIL_VNUM(found_abil), ABIL_COST_TYPE(found_abil), ABIL_COST(found_abil), NOTHING)) {
 		return;
 	}
-	if (ready_magic_weapon[type].cost_pool == BLOOD && !check_vampire_sun(ch, TRUE)) {
+	if (!ABILITY_FLAGGED(found_abil, ABILF_IGNORE_SUN) && ABIL_COST(found_abil) > 0 && ABIL_COST_TYPE(found_abil) == BLOOD && !check_vampire_sun(ch, TRUE)) {
 		return;
 	}
-	if (ABILITY_TRIGGERS(ch, NULL, NULL, ready_magic_weapon[type].ability)) {
+	if (ABILITY_TRIGGERS(ch, NULL, NULL, ABIL_VNUM(found_abil))) {
+		return;
+	}
+	
+	// determine wear position
+	if (CAN_WEAR(proto, ITEM_WEAR_WIELD)) {
+		pos = WEAR_WIELD;
+	}
+	else if (CAN_WEAR(proto, ITEM_WEAR_HOLD)) {
+		pos = WEAR_HOLD;	// ONLY if they can't wield it
+	}
+	else if (CAN_WEAR(proto, ITEM_WEAR_RANGED)) {
+		pos = WEAR_RANGED;
+	}
+	else {
+		log("SYSERR: %s trying to ready %d %s with no valid wear bits (Ability %d %s)", GET_NAME(ch), GET_OBJ_VNUM(proto), GET_OBJ_SHORT_DESC(proto), ABIL_VNUM(found_abil), ABIL_NAME(found_abil));
+		msg_to_char(ch, "You can't seem to ready that item.\r\n");
 		return;
 	}
 	
 	// if they are using a NON-1-use item, determine level now
-	if (GET_EQ(ch, WEAR_WIELD) && !OBJ_FLAGGED(GET_EQ(ch, WEAR_WIELD), OBJ_SINGLE_USE)) {
+	if (GET_EQ(ch, pos) && !OBJ_FLAGGED(GET_EQ(ch, pos), OBJ_SINGLE_USE)) {
 		ch_level = get_approximate_level(ch);
 		later = FALSE;
 	}
 	
-	// attempt to remove existing wield
-	if (GET_EQ(ch, WEAR_WIELD)) {
-		perform_remove(ch, WEAR_WIELD);
+	// attempt to remove existing item
+	if (GET_EQ(ch, pos)) {
+		perform_remove(ch, pos);
 		
 		// did it work? if not, player got an error
-		if (GET_EQ(ch, WEAR_WIELD)) {
+		if (GET_EQ(ch, pos)) {
 			return;
 		}
 	}
@@ -434,23 +505,22 @@ ACMD(do_ready) {
 		ch_level = get_approximate_level(ch);
 	}
 	
-	charge_ability_cost(ch, ready_magic_weapon[type].cost_pool, ready_magic_weapon[type].cost, NOTHING, 0, WAIT_SPELL);
+	charge_ability_cost(ch, ABIL_COST_TYPE(found_abil), ABIL_COST(found_abil), NOTHING, 0, WAIT_SPELL);
 	
 	// load the object
-	obj = read_object(ready_magic_weapon[type].vnum, TRUE);
-	abil = find_ability_by_vnum(ready_magic_weapon[type].ability);
-	if (!abil || IS_CLASS_ABILITY(ch, ready_magic_weapon[type].ability) || ABIL_ASSIGNED_SKILL(abil) == NULL) {
+	obj = read_object(GET_OBJ_VNUM(proto), TRUE);
+	if (IS_CLASS_ABILITY(ch, ABIL_VNUM(found_abil)) || ABIL_ASSIGNED_SKILL(found_abil) == NULL) {
 		scale_level = ch_level;	// class-level
 	}
 	else {
-		scale_level = MIN(ch_level, get_skill_level(ch, SKILL_VNUM(ABIL_ASSIGNED_SKILL(abil))));
+		scale_level = MIN(ch_level, get_skill_level(ch, SKILL_VNUM(ABIL_ASSIGNED_SKILL(found_abil))));
 	}
 	if (GET_SKILL_LEVEL(ch) < CLASS_SKILL_CAP) {	// ensure they'll be able to use the final item
 		scale_level = MIN(scale_level, GET_SKILL_LEVEL(ch));
 	}
 	scale_item_to_level(obj, scale_level);
 	
-	switch (ready_magic_weapon[type].cost_pool) {
+	switch (ABIL_COST_TYPE(found_abil)) {
 		case MANA: {
 			act("Mana twists and swirls around your hand and becomes $p!", FALSE, ch, obj, NULL, TO_CHAR);
 			act("Mana twists and swirls around $n's hand and becomes $p!", TRUE, ch, obj, NULL, TO_ROOM);
@@ -469,12 +539,10 @@ ACMD(do_ready) {
 		}
 	}
 	
-	equip_char(ch, obj, WEAR_WIELD);
+	equip_char(ch, obj, pos);
 	determine_gear_level(ch);
 	
-	if (ready_magic_weapon[type].ability != NO_ABIL) {
-		gain_ability_exp(ch, ready_magic_weapon[type].ability, 15);
-	}
+	gain_ability_exp(ch, ABIL_VNUM(found_abil), 15);
 	
 	load_otrigger(obj);
 }
