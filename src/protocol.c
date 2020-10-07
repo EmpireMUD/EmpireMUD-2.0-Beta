0b27a7fa658c30868dcfeffc460b3087799fe675
EmpireMUD <paul@empiremud.net>
2020-10-07 10:26:04 -0700

    Revert "test echo-off fix?"
    This reverts commit 4cc387c70f6d004e660810720e93fc1d74a4d55d.

--- src/protocol.c
+++ src/protocol.c
@@ -1196,9 +1196,7 @@ void ProtocolNegotiate(descriptor_t *apDescriptor) {
 
 /* Tells the client to switch echo on or off. */
 void ProtocolNoEcho(descriptor_t *apDescriptor, bool_t abOn) {
-	if ((abOn && apDescriptor->pProtocol->bECHO) || (!abOn && !apDescriptor->pProtocol->bECHO)) {
-		ConfirmNegotiation(apDescriptor, eNEGOTIATED_ECHO, abOn, true);
-	}
+	ConfirmNegotiation(apDescriptor, eNEGOTIATED_ECHO, abOn, true);
 }
 
 
