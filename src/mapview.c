66185a71efb07d78898862f99b6a37247e329a5e
EmpireMUD <paul@empiremud.net>
2020-10-11 10:53:11 -0700

    apply base color correctly on painted tiles

--- src/mapview.c
+++ src/mapview.c
@@ -1670,8 +1670,14 @@ static void show_map_to_char(char_data *ch, struct mappc_data_container *mappc, 
 		// buf is the icon
 		replace_color_codes(buf, col_buf);
 		
+		// check for ? colors
 		if (strstr(buf, "&?")) {
 			replace_question_color(buf, base_color, lbuf);
+			strcpy(buf, lbuf);
+		}
+		// need a leading color base color?
+		if (*buf != '&') {
+			snprintf(lbuf, sizeof(lbuf), "%s%s", base_color, buf);
 			strcpy(buf, lbuf);
 		}
 	}
