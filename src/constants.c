7fabd9d8278479bfbacef92bfae1222cbaf06751
EmpireMUD <paul@empiremud.net>
2022-03-07 07:51:15 -0700

    change scaling on some obj flags
    BOE, BOP, SUPERIOR, HARD-DROP, and GROUP-DROP

--- src/constants.c
+++ src/constants.c
@@ -2544,7 +2544,7 @@ const double obj_flag_scaling_bonus[] = {
 	1.1,	// OBJ_UNIQUE
 	1.0,	// OBJ_PLANTABLE
 	1.0,	// OBJ_LIGHT
-	1.6,	// OBJ_SUPERIOR
+	1.73,	// OBJ_SUPERIOR
 	1.0,	// OBJ_LARGE
 	1.0,	// OBJ_CREATED
 	1.0,	// OBJ_SINGLE_USE
@@ -2555,8 +2555,8 @@ const double obj_flag_scaling_bonus[] = {
 	1.0,	// OBJ_CREATABLE
 	1.0,	// OBJ_SCALABLE
 	1.5,	// OBJ_TWO_HANDED
-	1.3333,	// OBJ_BIND_ON_EQUIP
-	1.5,	// OBJ_BIND_ON_PICKUP
+	1.3,	// OBJ_BIND_ON_EQUIP
+	1.4,	// OBJ_BIND_ON_PICKUP
 	1.0,	// unused
 	1.0,	// OBJ_UNCOLLECTED_LOOT
 	1.0,	// OBJ_KEEP
@@ -2564,7 +2564,7 @@ const double obj_flag_scaling_bonus[] = {
 	1.0,	// unused
 	1.0,	// OBJ_NO_AUTOSTORE
 	1.2,	// OBJ_HARD_DROP
-	1.4,	// OBJ_GROUP_DROP
+	1.3333,	// OBJ_GROUP_DROP
 	1.0,	// OBJ_GENERIC_DROP
 	1.0,	// OBJ_NO_STORE
 	1.0,	// OBJ_SEEDED
