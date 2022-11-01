be307aa3b866936c72312578cba86a0be7a1f321
Paul Clarke <EmpireMUD@users.noreply.github.com>
2022-11-01 11:50:39 -0700

    fix possible null

--- src/generic.c
+++ src/generic.c
@@ -346,11 +346,11 @@ bool audit_generic(generic_data *gen, char_data *ch) {
 		case GENERIC_AFFECT: {
 			if (GET_AFFECT_LOOK_AT_ROOM(gen)) {
 				// only if present...
-				if (strncmp(GET_AFFECT_LOOK_AT_ROOM(gen), "...", 3)) {
+				if (strncmp(NULLSAFE(GET_AFFECT_LOOK_AT_ROOM(gen)), "...", 3)) {
 					olc_audit_msg(ch, GEN_VNUM(gen), "Look-at-room string does not begin with '...'");
 					problem = TRUE;
 				}
-				else if (!strncmp(GET_AFFECT_LOOK_AT_ROOM(gen), "... ", 4)) {
+				else if (!strncmp(NULLSAFE(GET_AFFECT_LOOK_AT_ROOM(gen)), "... ", 4)) {
 					olc_audit_msg(ch, GEN_VNUM(gen), "Look-at-room should not have a space after the '...'");
 					problem = TRUE;
 				}
