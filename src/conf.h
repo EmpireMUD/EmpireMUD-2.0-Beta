4b5236c38dd998fe97adadc797642751026d6c35
Paul Clarke <EmpireMUD@users.noreply.github.com>
2023-09-05 09:27:02 -0700

    text fixes

---
+++ src/conf.h
@@ -0,0 +1,6 @@
+
+/* Define if we're compiling EmpireMUD under any type of UNIX system.  */
+/* #undef EMPIRE_UNIX */
+
+/* Define if the system is capable of using crypt() to encrypt.  */
+#define EMPIRE_CRYPT 1
