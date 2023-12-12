99296d9d93fa3b02caf2bad65a883f5276f256cc
Paul Clarke <EmpireMUD@users.noreply.github.com>
2023-12-11 19:10:45 -0700

    fix over-time abilities

--- src/abilities.c
+++ src/abilities.c
@@ -2779,7 +2779,7 @@ void perform_over_time_ability(char_data *ch) {
 	if (vict && vict != ch && has_custom_message_pos(ABIL_CUSTOM_MSGS(abil), ABIL_CUSTOM_TARGETED_TO_CHAR, GET_ACTION_TIMER(ch) + 1)) {
 		// ongoing ability: do nothing
 	}
-	else if (!vict && has_custom_message_pos(ABIL_CUSTOM_MSGS(abil), ABIL_CUSTOM_SELF_TO_CHAR, GET_ACTION_TIMER(ch) + 1)) {
+	else if ((!vict || vict != ch) && has_custom_message_pos(ABIL_CUSTOM_MSGS(abil), ABIL_CUSTOM_SELF_TO_CHAR, GET_ACTION_TIMER(ch) + 1)) {
 		// ongoing ability: do nothing
 	}
 	else {
