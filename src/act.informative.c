f4867b485947acab7cf22a4824c3cc2f51735b11
Paul Clarke <EmpireMUD@users.noreply.github.com>
2023-12-21 10:56:26 -0700

    fix imm display for blank affs

--- src/act.informative.c
+++ src/act.informative.c
@@ -2414,6 +2414,7 @@ ACMD(do_affects) {
 			send_config_msg(ch, "no_person");
 		}
 		else if (IS_IMMORTAL(ch)) {
+			msg_to_char(ch, "Affects on %s:\r\n", PERS(vict, ch, FALSE));
 			show_character_affects(vict, ch);
 		}
 		else {
