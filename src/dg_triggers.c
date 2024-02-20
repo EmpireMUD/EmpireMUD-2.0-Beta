72565d34b4373f0403a5580189435ae9b23a3728
Paul Clarke <EmpireMUD@users.noreply.github.com>
2024-02-20 10:25:40 -0700

    drop, give, and remove triggers now respect nohassle

--- src/dg_triggers.c
+++ src/dg_triggers.c
@@ -2024,7 +2024,7 @@ int remove_otrigger(obj_data *obj, char_data *actor) {
 	char buf[MAX_INPUT_LENGTH];
 	int ret_val = 1;
 
-	if (!SCRIPT_CHECK(obj, OTRIG_REMOVE)) {
+	if (!SCRIPT_CHECK(obj, OTRIG_REMOVE) || !valid_dg_target(actor, DG_ALLOW_GODS)) {
 		return 1;
 	}
 
@@ -2071,7 +2071,7 @@ int drop_otrigger(obj_data *obj, char_data *actor, int mode) {
 	char buf[MAX_INPUT_LENGTH];
 	int ret_val = 1;
 
-	if (!SCRIPT_CHECK(obj, OTRIG_DROP)) {
+	if (!SCRIPT_CHECK(obj, OTRIG_DROP) || !valid_dg_target(actor, DG_ALLOW_GODS)) {
 		return 1;
 	}
 
@@ -2142,7 +2142,7 @@ int give_otrigger(obj_data *obj, char_data *actor, char_data *victim) {
 	char buf[MAX_INPUT_LENGTH];
 	int ret_val = 1;
 
-	if (!SCRIPT_CHECK(obj, OTRIG_GIVE)) {
+	if (!SCRIPT_CHECK(obj, OTRIG_GIVE) || !valid_dg_target(actor, DG_ALLOW_GODS)) {
 		return 1;
 	}
 
