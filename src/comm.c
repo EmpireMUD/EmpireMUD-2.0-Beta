78ee6f7436fbede0c25e1f8088dc31ba5f856d9a
Paul Clarke <EmpireMUD@users.noreply.github.com>
2024-03-06 14:38:00 -0700

    remove cancel-over-time actions on shutdown/reboot

--- src/comm.c
+++ src/comm.c
@@ -669,11 +669,6 @@ void perform_reboot(void) {
 			}
 		}
 		
-		if (GET_ACTION(desc->character) == ACT_OVER_TIME_ABILITY) {
-			// these are not reboot-safe
-			cancel_action(desc->character);
-		}
-		
 		// send output
 		write_to_descriptor(desc->descriptor, buf);
 		if (reboot_control.type == REBOOT_REBOOT) {
