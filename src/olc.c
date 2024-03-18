c0f7bf03ff6dde76c933e212fdc5678de2782c9a
Paul Clarke <EmpireMUD@users.noreply.github.com>
2024-03-17 17:36:13 -0700

    add event fullsearch

--- src/olc.c
+++ src/olc.c
@@ -1185,7 +1185,7 @@ const struct olc_command_data olc_data[] = {
 	
 	
 	// misc commands that should not take precedence over editor commands
-	{ "fullsearch", olc_fullsearch, OLC_ABILITY | OLC_ADVENTURE | OLC_ARCHETYPE | OLC_ATTACK | OLC_AUGMENT | OLC_BOOK | OLC_BUILDING | OLC_CRAFT | OLC_CROP | OLC_GENERIC | OLC_MOBILE |  OLC_OBJECT | OLC_PROGRESS | OLC_QUEST | OLC_ROOM_TEMPLATE | OLC_SECTOR | OLC_SKILL | OLC_TRIGGER | OLC_VEHICLE, NOBITS },
+	{ "fullsearch", olc_fullsearch, OLC_ABILITY | OLC_ADVENTURE | OLC_ARCHETYPE | OLC_ATTACK | OLC_AUGMENT | OLC_BOOK | OLC_BUILDING | OLC_CRAFT | OLC_CROP | OLC_EVENT | OLC_GENERIC | OLC_MOBILE |  OLC_OBJECT | OLC_PROGRESS | OLC_QUEST | OLC_ROOM_TEMPLATE | OLC_SECTOR | OLC_SKILL | OLC_TRIGGER | OLC_VEHICLE, NOBITS },
 	
 	// this goes last
 	{ "\n", NULL, NOBITS, NOBITS }
@@ -2820,6 +2820,11 @@ OLC_MODULE(olc_fullsearch) {
 		case OLC_CROP: {
 			void olc_fullsearch_crop(char_data *ch, char *argument);
 			olc_fullsearch_crop(ch, argument);
+			break;
+		}
+		case OLC_EVENT: {
+			void olc_fullsearch_event(char_data *ch, char *argument);
+			olc_fullsearch_event(ch, argument);
 			break;
 		}
 		case OLC_GENERIC: {
