ee822376eaa64f115f7eb624948583a4a977e0ad
Paul Clarke <EmpireMUD@users.noreply.github.com>
2026-05-12 10:14:07 -0700

    add multiple links at once

--- src/olc.trigger.c
+++ src/olc.trigger.c
@@ -1177,11 +1177,11 @@ OLC_MODULE(tedit_links) {
 	trig_data *trig = GET_OLC_TRIGGER(ch->desc);
 	any_vnum vnum;
 	bool any;
-	char cmd_arg[MAX_INPUT_LENGTH], type_arg[MAX_INPUT_LENGTH], vnum_arg[MAX_INPUT_LENGTH], buf[MAX_STRING_LENGTH];
+	char cmd_arg[MAX_INPUT_LENGTH], type_arg[MAX_INPUT_LENGTH], vnum_arg[MAX_INPUT_LENGTH], one_vnum[MAX_INPUT_LENGTH], buf[MAX_STRING_LENGTH], temp[MAX_INPUT_LENGTH];
 	int pos;
 	struct trig_link *link, *next_link;
 	
-	const char *usage = "Usage: links add <type> <vnum>\r\n"
+	const char *usage = "Usage: links add <type> <vnum(s)>\r\n"
 						"       links remove <type> <vnum | all>\r\n"
 						"       links remove all\r\n";
 	
@@ -1193,33 +1193,46 @@ OLC_MODULE(tedit_links) {
 		msg_to_char(ch, "%s", usage);
 	}
 	else if (is_abbrev(cmd_arg, "add")) {
-		if (!*vnum_arg || !isdigit(*vnum_arg) || (vnum = atoi(vnum_arg)) < 0) {
+		if (!*vnum_arg || !isdigit(*vnum_arg)) {
 			msg_to_char(ch, "%s", usage);
 		}
 		else if ((pos = search_block(type_arg, olc_type_bits, FALSE)) == NOTHING) {
 			msg_to_char(ch, "Unknown type '%s'.\r\n", type_arg);
 		}
 		else {
-			// ok to add -- first ensure not already in list
-			any = FALSE;
-			LL_FOREACH(GET_TRIG_LINKS(trig), link) {
-				if (IS_SET(link->type, BIT(pos)) && link->vnum == vnum) {
-					any = TRUE;
-					break;
-				}
-			}
-			
+			// for messaging
 			prettier_sprintbit(BIT(pos), olc_type_bits, buf);
-			if (any) {
-				msg_to_char(ch, "Link already exists to %s [%d] %s.\r\n", buf, vnum, get_name_by_olc_type(BIT(pos), vnum));
-			}
-			else {
-				CREATE(link, struct trig_link, 1);
-				link->type = BIT(pos);
-				link->vnum = vnum;
-				LL_PREPEND(GET_TRIG_LINKS(trig), link);
-				LL_SORT(GET_TRIG_LINKS(trig), sort_trigger_links);
-				msg_to_char(ch, "Link added for %s [%d] %s.\r\n", buf, vnum, get_name_by_olc_type(BIT(pos), vnum));
+			
+			// add mulitple entries
+			while (*vnum_arg) {
+				// accept multiple vnums: peel 1 off
+				half_chop(vnum_arg, one_vnum, temp);
+				strcpy(vnum_arg, temp);
+				if (!isdigit(*one_vnum) || (vnum = atoi(one_vnum)) < 0) {
+					msg_to_char(ch, "Invalid vnum '%s'.\r\n", one_vnum);
+					continue;	// try the rest anyway
+				}
+				
+				// ok to add -- first ensure not already in list
+				any = FALSE;
+				LL_FOREACH(GET_TRIG_LINKS(trig), link) {
+					if (IS_SET(link->type, BIT(pos)) && link->vnum == vnum) {
+						any = TRUE;
+						break;
+					}
+				}
+				
+				if (any) {
+					msg_to_char(ch, "Link already exists to %s [%d] %s.\r\n", buf, vnum, get_name_by_olc_type(BIT(pos), vnum));
+				}
+				else {
+					CREATE(link, struct trig_link, 1);
+					link->type = BIT(pos);
+					link->vnum = vnum;
+					LL_PREPEND(GET_TRIG_LINKS(trig), link);
+					LL_SORT(GET_TRIG_LINKS(trig), sort_trigger_links);
+					msg_to_char(ch, "Link added for %s [%d] %s.\r\n", buf, vnum, get_name_by_olc_type(BIT(pos), vnum));
+				}
 			}
 		}
 	}
