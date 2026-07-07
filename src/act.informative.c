0408dfbe396cb0d356632e00913d69286a9c1e6e
Paul Clarke <EmpireMUD@users.noreply.github.com>
2026-07-06 19:28:38 -0700

    add percent sign

--- src/act.informative.c
+++ src/act.informative.c
@@ -4733,7 +4733,7 @@ ACMD(do_survey) {
 			maxhealth = GET_BUILDING(IN_ROOM(ch)) ? GET_BLD_MAX_DAMAGE(GET_BUILDING(IN_ROOM(ch))) : 1;
 			maxhealth = MAX(1, maxhealth);	// don't crash me, bro
 			health = (1.0 - (double) BUILDING_DAMAGE(IN_ROOM(ch)) / maxhealth) * 100.0;
-			msg_to_char(ch, "It's in need of maintenance and repair (%d damaged).\r\n", (int)round(health));
+			msg_to_char(ch, "It's in need of maintenance and repair (%d%% damaged).\r\n", (int)round(health));
 		}
 		if (IS_BURNING(IN_ROOM(ch))) {
 			msg_to_char(ch, "It's on fire!\r\n");
