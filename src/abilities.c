3185eeb1375f3fbf69be785bd8cc0aac5e975554
Paul Clarke <EmpireMUD@users.noreply.github.com>
2026-07-22 07:12:45 -0700

    fix in-city ability limit

--- src/abilities.c
+++ src/abilities.c
@@ -3387,7 +3387,7 @@ bool check_ability_limitations(char_data *ch, ability_data *abil, char_data *vic
 			}
 			case ABIL_LIMIT_IN_CITY: {
 				bool wait = FALSE;
-				if (!ROOM_OWNER(any_room) && !is_in_city_for_empire(any_room, ROOM_OWNER(any_room), TRUE, &wait)) {
+				if (!ROOM_OWNER(any_room) || !is_in_city_for_empire(any_room, ROOM_OWNER(any_room), TRUE, &wait)) {
 					msg_to_char(ch, "You must be in a city to use that ability%s.\r\n", wait ? " (this city was founded too recently)" : "");
 					_set_fatal_error(TRUE);
 					return FALSE;
