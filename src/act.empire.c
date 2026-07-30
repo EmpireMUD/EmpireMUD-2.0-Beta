d4105b5d033c1629d38d56e542e56310e2d09375
Paul Clarke <EmpireMUD@users.noreply.github.com>
2026-07-30 07:12:20 -0700

    fix count on territory list

--- src/act.empire.c
+++ src/act.empire.c
@@ -8249,7 +8249,8 @@ ACMD(do_territory) {
 		
 			// final ok: add to the list
 			if (ok && (node = find_territory_node_in_hash(&node_hash, IN_ROOM(veh), TRUE))) {
-				++(node->count);
+				// only ensure at least 1 on the count; do not add at this point
+				node->count = MAX(1, node->count);
 				
 				// add notes
 				sprintf(buf, "%s%s%s", NULLSAFE(node->details), (node->details ? ", " : ""), skip_filler(VEH_SHORT_DESC(veh)));
@@ -8346,7 +8347,7 @@ ACMD(do_territory) {
 		
 		// final ok: add to the list
 		if (ok && (node = find_territory_node_in_hash(&node_hash, iter, TRUE))) {
-			// only ensure at least 1 on the count; do not add if the node is present from a vehicle
+			// only ensure at least 1 on the count; do not add at this point
 			node->count = MAX(1, node->count);
 			
 			// mark as interior?
