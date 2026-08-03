0d28979b6080b945e7529b02eeae2f3ab6da8f12
Paul Clarke <EmpireMUD@users.noreply.github.com>
2026-08-03 10:39:02 -0700

    show required climate better

--- src/building.c
+++ src/building.c
@@ -216,7 +216,13 @@ bool check_build_location_and_dir(char_data *ch, room_data *room, craft_data *ty
 	}
 	if (make_veh && (is_upgrade || !ROOM_IS_CLOSED(room)) && !vehicle_allows_climate(make_veh, room, NULL)) {
 		if (ch) {
-			msg_to_char(ch, "You can't %s %s here due to the climate or terrain.\r\n", command, get_vehicle_short_desc(make_veh, ch));
+			if (VEH_REQUIRES_CLIMATE(make_veh)) {
+				ordered_sprintbit(VEH_REQUIRES_CLIMATE(make_veh), climate_flags, climate_flags_order, FALSE, buf);
+				msg_to_char(ch, "You can't %s %s here due to the climate or terrain (requires %s).\r\n", command, get_vehicle_short_desc(make_veh, ch), buf);
+			}
+			else {
+				msg_to_char(ch, "You can't %s %s here due to the climate or terrain.\r\n", command, get_vehicle_short_desc(make_veh, ch));
+			}
 		}
 		return FALSE;
 	}
