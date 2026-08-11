c6ecb46403a46fefb5710e4f18db1dc7ca4c63dc
Paul Clarke <EmpireMUD@users.noreply.github.com>
2026-08-11 06:46:52 -0700

    vehicle chameleon fix

--- src/vehicles.c
+++ src/vehicles.c
@@ -1526,8 +1526,8 @@ bool vehicle_is_chameleon(vehicle_data *veh, room_data *from) {
 	if (!veh || !from || !IN_ROOM(veh)) {
 		return FALSE;	// safety
 	}
-	if (!VEH_IS_COMPLETE(veh) || VEH_NEEDS_RESOURCES(veh)) {
-		return FALSE;	// incomplete or unrepaired vehicles are not chameleon
+	if (!VEH_IS_COMPLETE(veh)) {
+		return FALSE;	// incomplete vehicles are not chameleon
 	}
 	if ((!VEH_FLAGGED(veh, VEH_CHAMELEON) || VEH_HAS_MAJOR_DISREPAIR(veh)) && (!IN_ROOM(veh) || !CHECK_CHAMELEON(from, IN_ROOM(veh)))) {
 		return FALSE;	// missing chameleon flags, or damaged
