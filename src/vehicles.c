29235333a859c5b381a092e95f5f6039788d5d75
Paul Clarke <EmpireMUD@users.noreply.github.com>
2026-08-11 06:45:00 -0700

    vehicle chameleon stops working with major disrepair

--- src/vehicles.c
+++ src/vehicles.c
@@ -1529,8 +1529,8 @@ bool vehicle_is_chameleon(vehicle_data *veh, room_data *from) {
 	if (!VEH_IS_COMPLETE(veh) || VEH_NEEDS_RESOURCES(veh)) {
 		return FALSE;	// incomplete or unrepaired vehicles are not chameleon
 	}
-	if (!VEH_FLAGGED(veh, VEH_CHAMELEON) && (!IN_ROOM(veh) || !CHECK_CHAMELEON(from, IN_ROOM(veh)))) {
-		return FALSE;	// missing chameleon flags
+	if ((!VEH_FLAGGED(veh, VEH_CHAMELEON) || VEH_HAS_MAJOR_DISREPAIR(veh)) && (!IN_ROOM(veh) || !CHECK_CHAMELEON(from, IN_ROOM(veh)))) {
+		return FALSE;	// missing chameleon flags, or damaged
 	}
 	
 	// ok chameleon: now check distance
